a2bca7bf7a7d8f14cc9577f5bf59fc1ad9793cea
zp <zp_nic@163.com>
2020-01-06 17:04:56 +0800

    增加出版物页面

--- src/components/publications.vue
+++ src/components/publications.vue
@@ -1,15 +1,49 @@
 <template>
-  <div>
-    publications
+  <div class="block">
+    <el-row :gutter="20">
+      <el-col :span="12" :offset="6">
+        <el-timeline>
+          <el-timeline-item
+            v-for="(activity, index) in pubs"
+            :key="index"
+            :timestamp="activity.date" placement="top">
+            <el-card>
+              <h4>{{activity.title}}</h4>
+              <p>{{activity.info}}</p>
+              <p>{{activity.authors}}</p>
+            </el-card>
+          </el-timeline-item>
+        </el-timeline>
+      </el-col>
+    </el-row>
+
   </div>
 </template>
 
 <script>
-    export default {
-        name: "publications"
+  export default {
+    name: "publications",
+    data() {
+      return {
+        pubs: []
+      }
+    },
+    mounted() {
+      this.getPubs()
+    },
+    methods: {
+      getPubs() {
+          this.$axios.get('http://localhost:8080/static/publications.json').then((res) => {
+            this.pubs = res.data.pubs
+          })
+      }
     }
+  }
 </script>
 
 <style scoped>
-
+  .kanwu {
+    font-weight: bold;
+    font-style: italic;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import axios from 'axios'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
@@ -8,6 +9,7 @@ import router from './router'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+Vue.prototype.$axios = axios    //全局注册，使用方法为:this.$axios
 
 /* eslint-disable no-new */
 new Vue({
