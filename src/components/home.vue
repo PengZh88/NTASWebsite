97ee3182543a97ea0907ce9e9cf580528f8915bf
zp_nic@163.com <123321qwe>
2021-04-11 00:06:02 +0800

    更新主页

--- src/components/home.vue
+++ src/components/home.vue
@@ -11,7 +11,7 @@
           <el-menu-item index="/index">
             <span slot="title">Home</span>
           </el-menu-item>
-          <el-menu-item index="/datasets">Data sets</el-menu-item>
+          <el-menu-item index="/datasets">Datasets</el-menu-item>
           <el-menu-item index="/algorithms">Algorithms</el-menu-item>
           <el-menu-item index="/researchers">Researchers</el-menu-item>
           <el-menu-item index="/publications">Publications</el-menu-item>
@@ -23,15 +23,59 @@
       <el-main>
         <div v-if="isHome">
           <el-row :gutter="20">
-            <el-col :span="16" :offset="4">
-              <el-carousel trigger="click">
+            <el-col :span="20" :offset="2">
+              <el-carousel trigger="click" height="450px">
                 <el-carousel-item v-for="item in 4" :key="item">
                   <el-image
-                    style="width: 100%"
+                    style="width: 100%; height: 100%; "
                     :src="meimgs"
                     :fit="fit"></el-image>
                 </el-carousel-item>
               </el-carousel>
+            </el-col>
+          </el-row>
+          <el-row :gutter="20">
+            <el-col :span="20" :offset="2">
+              <el-card class="box-card" shadow="hover">
+                <div slot="header" class="card-header">
+                  <span>Works and Papers</span>
+                </div>
+                <div class="card-content">
+                  <p>This website is an ongoing project to develop a comprehensive system for research into analysis of IPv6 network traffic data based on time series data mining (CERNET Innovation Project NGII20190109).</p>
+                </div>
+              </el-card>
+            </el-col>
+          </el-row>
+          <el-row :gutter="20">
+            <el-col :span="20" :offset="2">
+              <el-card class="box-card" shadow="hover">
+                <div slot="header" class="card-header">
+                  <span>Codes</span>
+                </div>
+                <div class="card-content">
+                  <p>There are two code repositories associated with this website.</p>
+                  <ul>
+                    <li>
+                      Frontend Code: <a :href="frontend" target="_blank">{{ frontend }}</a>
+                    </li>
+                    <li>
+                      Backend Code: <a :href="backend" target="_blank">{{ backend }}</a>
+                    </li>
+                  </ul>
+                </div>
+              </el-card>
+            </el-col>
+          </el-row>
+          <el-row :gutter="20">
+            <el-col :span="20" :offset="2">
+              <el-card class="box-card" shadow="hover">
+                <div slot="header" class="card-header">
+                  <span>Datasets</span>
+                </div>
+                <div class="card-content">
+                  <p>For detail, please refer to <a :href="datasetsurl">Data sets</a>.</p>
+                </div>
+              </el-card>
             </el-col>
           </el-row>
         </div>
@@ -53,7 +97,10 @@
         msg: '',
         activeIndex: '/index',
         isHome: true,
-        meimgs: meimgs
+        meimgs: meimgs,
+        frontend: 'https://github.com/PengZh88/cernet-web',
+        backend: 'https://github.com/PengZh88/cernet-back',
+        datasetsurl: '/datasets'
       }
     },
     created() {
@@ -96,5 +143,29 @@
 
   .el-carousel__item:nth-child(2n+1) {
     background-color: #d3dce6;
+  }
+
+  .card-header {
+    text-align: left;
+    color: #2e6da4;
+    font-size: 16pt;
+  }
+
+  .card-content {
+    font-size: 14pt;
+    margin: 0 auto;
+    text-align: left;
+  }
+
+  .card-content > p {
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    display: block;
+  }
+
+  ul > li {
+    margin-top: 12px;
   }
 </style>
