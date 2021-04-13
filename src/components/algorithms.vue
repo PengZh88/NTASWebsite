b552cfde27ab39ef8fc89683e030bdebe83f0ca7
zp_nic@163.com <123321qwe>
2021-04-13 22:24:47 +0800

    更新代码栏目内容

--- src/components/algorithms.vue
+++ src/components/algorithms.vue
@@ -29,7 +29,7 @@
           </el-table-column>
           <el-table-column
             label="Year"
-            prop="algyear">
+            prop="year">
           </el-table-column>
           <el-table-column
             align="right">
@@ -54,9 +54,116 @@
     <el-dialog
       :title="diaTitle"
       :visible.sync="dialogVisible"
-      width="30%"
-      :before-close="handleClose" @open="diaOpen">
-      <span>这是一段信息</span>
+      width="40%"
+      :closed="handleClose" @open="diaOpen">
+      <el-row>
+        <el-col :span="6">
+          <div class="grid-content bg-purple">
+            Title
+          </div>
+        </el-col>
+        <el-col :span="18">
+          <div class="grid-content bg-purple-light">
+            {{ ptitle }}
+          </div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="6">
+          <div class="grid-content bg-purple">
+            Authors
+          </div>
+        </el-col>
+        <el-col :span="18">
+          <div class="grid-content bg-purple-light">
+            {{ pauthors }}
+          </div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="6">
+          <div class="grid-content bg-purple">
+            Type
+          </div>
+        </el-col>
+        <el-col :span="18">
+          <div class="grid-content bg-purple-light">
+            {{ ptype }}
+          </div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="6">
+          <div class="grid-content bg-purple">
+            Publication
+          </div>
+        </el-col>
+        <el-col :span="18">
+          <div class="grid-content bg-purple-light">
+            {{ ppublication }}
+          </div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="6">
+          <div class="grid-content bg-purple">
+            Volume
+          </div>
+        </el-col>
+        <el-col :span="18">
+          <div class="grid-content bg-purple-light">
+            {{ pvolume }}
+          </div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="6">
+          <div class="grid-content bg-purple">
+            Pages
+          </div>
+        </el-col>
+        <el-col :span="18">
+          <div class="grid-content bg-purple-light">
+            {{ ppages }}
+          </div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="6">
+          <div class="grid-content bg-purple">
+            Year
+          </div>
+        </el-col>
+        <el-col :span="18">
+          <div class="grid-content bg-purple-light">
+            {{ pyear }}
+          </div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="6">
+          <div class="grid-content bg-purple">
+            DOI
+          </div>
+        </el-col>
+        <el-col :span="18">
+          <div class="grid-content bg-purple-light">
+            {{ pdoi }}
+          </div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="6">
+          <div class="grid-content bg-purple">
+            URL
+          </div>
+        </el-col>
+        <el-col :span="18">
+          <div class="grid-content bg-purple-light">
+            <a :href="purl" target="_blank">{{ purl }}</a>
+          </div>
+        </el-col>
+      </el-row>
       <span slot="footer" class="dialog-footer">
         <el-button type="primary" @click="dialogVisible = false">Close</el-button>
       </span>
@@ -69,56 +176,60 @@ export default {
   name: "algorithms",
   data() {
     return {
-      tableData: [{
-        alg: 'MSPAR-AD',
-        algtype: 'Distance Based',
-        algyear: '2020',
-        authors: '',
-        publications: ''
-      }, {
-        alg: 'KTPC-AD',
-        algtype: 'Clipped Distance Based',
-        algyear: '2020',
-        authors: '',
-        publications: ''
-      }, {
-        alg: 'EMR',
-        algtype: 'Multi-resolution',
-        algyear: '2020',
-        authors: '',
-        publications: ''
-      }, {
-        alg: 'EST',
-        algtype: 'Shapelets Based',
-        algyear: '2020',
-        authors: '',
-        publications: ''
-      }, {
-        alg: 'TORRENT',
-        algtype: 'Deep Learning',
-        algyear: '2020',
-        authors: '',
-        publications: ''
-      }, {
-        alg: 'FCAD',
-        algtype: 'Clipped Distance Based',
-        algyear: '2020',
-        authors: '',
-        publications: ''
-      }],
+      tableData: [],
       search: '',
       dialogVisible: false,
-      diaTitle: 'Dialog Title'
+      diaTitle: 'Dialog Title',
+      ptitle: '',
+      pauthors: '',
+      ptype: '',
+      ppublication: '',
+      pvolume: '',
+      ppages: '',
+      pyear: '',
+      pdoi: '',
+      purl: ''
     }
   },
+  mounted() {
+    this.getAlgs()
+  },
   methods: {
+    getAlgs: function() {
+      this.$axios.get('/static/algorithms.json').then((res) => {
+        this.tableData = res.data.algorithmdatas
+      })
+    },
     handleEdit(index, row) {
       console.log(index, row);
       this.dialogVisible = true;
       this.diaTitle = row.alg;
+      this.ptitle = row.title
+      this.ptype = row.algtype
+      this.ppublication = row.publication
+      this.pauthors = row.authors
+      this.pvolume = row.volume
+      this.ppages = row.pages
+      this.pyear = row.year
+      this.pdoi = row.doi
+      this.purl = row.url
     },
-    diaOpen: function() {
+    diaOpen: function () {
       console.log("I am dialog, I am opened!");
+    },
+    handleClose: function() {
+      this.clearDialog()
+    },
+    clearDialog: function() {
+      this.ptitle = ''
+      this.ptype = ''
+      this.ppublication = ''
+      this.pauthors = ''
+      this.pvolume = ''
+      this.ppages = ''
+      this.pyear = ''
+      this.pdoi = ''
+      this.purl = ''
     }
   }
 }
@@ -143,4 +254,36 @@ body > .el-container {
   width: 80%;
   margin: auto;
 }
+
+
+.el-col {
+  border-radius: 4px;
+}
+
+.bg-purple-dark {
+  background: #99a9bf;
+}
+
+.bg-purple {
+  background: #d3dce6;
+}
+
+.bg-purple-light {
+  background: #e5e9f2;
+}
+
+.grid-content {
+  border-radius: 4px;
+  word-wrap: break-word;
+  word-break: normal;
+  height: 40px;
+  padding-top: 10px;
+  font-size: 12pt;
+}
+
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
+
 </style>
